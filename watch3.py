2a1107ffd997bb2e2c5300811d731e0e2c3f0e39
BeeFrog <Bee@Frogm.com>
2020-07-31 20:17:22 +0100

    v3.1

--- watch3.py
+++ watch3.py
@@ -8,11 +8,13 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.add_event_detect(pin, GPIO.RISING)  # add rising edge detection on a channel
-
+print('Running... Waiting for trigger')
 start = time.time()
 while True:
     if GPIO.event_detected(pin):
-        print('switch triggered')    
+        print('switch triggered')
+        start = time.time()
     if time.time() - start > 5:
-        print('Timeout')
+        print('5 seconds elapsed with no trigger')
+        start = time.time()
     time.sleep(0.0001)
